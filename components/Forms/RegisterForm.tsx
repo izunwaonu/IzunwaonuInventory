d351f899124e0a896c71369933bd13d1ce46bff5
swaat <izunwaonu2@gmail.com>
2025-03-20 15:13:34 +0700

    updated logo in login and register page

--- components/Forms/RegisterForm.tsx
+++ components/Forms/RegisterForm.tsx
@@ -97,9 +97,9 @@ export default function RegisterForm() {
     <div className="w-full lg:grid h-screen lg:min-h-[600px] lg:grid-cols-2 relative ">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid  gap-6 mt-10 md:mt-0">
-          {/* <div className="absolute left-1/3 top-14 md:top-5 md:left-5">
+          <div className="absolute left-1/3 top-14 md:top-5 md:left-5">
             <Logo />
-          </div> */}
+          </div> 
           <div className="grid gap-2  mt-10 md:mt-0">
             <h1 className="text-3xl font-bold">Create an account</h1>
             <p className="text-muted-foreground text-sm">
