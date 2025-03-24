3d3f63044af3cfcd3f6919341ccdbffad0869845
swaat <izunwaonu2@gmail.com>
2025-03-24 12:18:25 +0700

    updated the modal form for user invitation

--- app/(dashboard)/dashboard/users/page.tsx
+++ app/(dashboard)/dashboard/users/page.tsx
@@ -2,17 +2,31 @@ import DataTable from "@/components/DataTableComponents/DataTable";
 import TableHeader from "@/components/dashboard/Tables/TableHeader";
 import { columns } from "./columns";
 import { getAllUsers } from "@/actions/users";
+import ModalTableHeader from "@/components/dashboard/Tables/ModalTableHeader";
+import { UserInvitationForm } from "@/components/Forms/users/UserInvitationForm.";
+import { getAuthenticatedUser } from "@/config/useAuth";
+import { getRoles } from "@/actions/roles";
 
 export default async function page() {
   const users = (await getAllUsers()) || [];
+  const user = await getAuthenticatedUser();
+  const res = await getRoles();
+  const rolesData = res.data || [];
+  const roles = rolesData.map((role) => ({ 
+    label: role.displayName, 
+    value: role.id
+  }))
+  const orgId = user.orgId;
+  const orgName = user?.orgName??"";
   return (
     <div className="p-8">
-      <TableHeader
+      <ModalTableHeader
         title="Users"
         linkTitle="Add User"
         href="/dashboard/users/new"
         data={users}
         model="user"
+        modalForm={<UserInvitationForm roles={roles} orgId={orgId} orgName={orgName}/>}
       />
       {/* <CustomDataTable categories={categories} /> */}
       <DataTable columns={columns} data={users} />
