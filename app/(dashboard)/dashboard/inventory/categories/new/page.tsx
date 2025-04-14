d38d2dee96d736f31ee76ac5e490ff89eae9ca64
swaat <izunwaonu2@gmail.com>
2025-04-14 18:03:13 +0700

    updated

--- app/(dashboard)/dashboard/inventory/categories/new/page.tsx
+++ app/(dashboard)/dashboard/inventory/categories/new/page.tsx
@@ -4,7 +4,8 @@ import React from "react";
 export default function page() {
   return (
     <div className="p-8">
-      <CategoryForm />
+      {/* <CategoryForm /> */}
+      <h1>Category form</h1>
     </div>
   );
 }
