0eefd7904c319590b92b9f2dacc3324a4e4ac9a9
swaat <izunwaonu2@gmail.com>
2025-06-03 15:49:13 +0700

    updated

--- app/(dashboard)/dashboard/inventory/items/[id]/edit/layout.tsx
+++ app/(dashboard)/dashboard/inventory/items/[id]/edit/layout.tsx
@@ -2,6 +2,6 @@ import { checkPermission } from '@/config/useAuth';
 import React, { ReactNode } from 'react';
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  await checkPermission('items.edit');
+  await checkPermission('items.read');
   return <div>{children}</div>;
 }
--- app/(dashboard)/dashboard/inventory/items/layout.tsx
+++ app/(dashboard)/dashboard/inventory/items/layout.tsx
@@ -1,7 +1,7 @@
-import { checkPermission } from "@/config/useAuth";
-import React, { ReactNode } from "react";
+import { checkPermission } from '@/config/useAuth';
+import React, { ReactNode } from 'react';
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  await checkPermission("categories.read");
+  await checkPermission('categories.read');
   return <div>{children}</div>;
 }
